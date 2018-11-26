933f2a82b37ae1015b58308ab24fe2478400b074
Mauricio Correa <frontend.mcu@gmail.com>
2018-11-26 23:02:11 +0100

    artistDetails

--- js/main.js
+++ js/main.js
@@ -1,3 +1,89 @@
+function artistDetails(id) {
+
+  const spinner = document.querySelector('.spinner');
+  spinner.style.display = 'block';
+
+  callApis('https://api.songkick.com/api/3.0/artists/' + id + '/calendar.json?apikey=' + config.API_SK, function (data) {
+    console.log(data);
+
+    spinner.style.display = 'none';
+
+    const listOfEvents = data.results.event;
+
+    const container = document.querySelector('.container');
+    container.innerHTML = '';
+
+    const eventDetails = document.createElement('DIV');
+    eventDetails.setAttribute('class', 'event-details');
+    container.appendChild(eventDetails);
+
+    const eventTotalSpan = document.createElement('SPAN');
+    eventTotalSpan.setAttribute('class', 'event-total');
+    eventTotalSpan.textContent = data.totalEntries + ' resultados encontrados';
+    eventDetails.appendChild(eventTotalSpan);
+
+    const ul = document.createElement('UL');
+    ul.setAttribute('id', 'events');
+    eventDetails.appendChild(ul);
+
+    listOfEvents.forEach(function (item) {
+
+      const li = document.createElement('LI');
+
+      const div1 = document.createElement('DIV');
+      div1.setAttribute('class', 'event-date');
+      const eventDateYear = document.createElement('DIV');
+      eventDateYear.setAttribute('class', 'event-date-year');
+      const eventDateMonth = document.createElement('DIV');
+      eventDateMonth.setAttribute('class', 'event-date-month');
+      const eventDateDay = document.createElement('DIV');
+      eventDateDay.setAttribute('class', 'event-date-day');
+
+      const div2 = document.createElement('DIV');
+      div2.setAttribute('class', 'event-date-details');
+      const eventDetailsHour = document.createElement('DIV');
+      eventDetailsHour.setAttribute('class', 'event-date-details-hour');
+      const eventDetailsVenue = document.createElement('DIV');
+      eventDetailsVenue.setAttribute('class', 'event-date-details-venue');
+      const eventCityCountry = document.createElement('DIV');
+      eventCityCountry.setAttribute('class', 'event-date-city-country');
+
+      const div3 = document.createElement('DIV');
+      div3.setAttribute('class', 'event-location');
+      const button = document.createElement('BUTTON');
+      button.appendChild(document.createTextNode('Buscar en el mapa'));
+
+      div1.appendChild(eventDateDay);
+      div1.appendChild(eventDateMonth);
+      div1.appendChild(eventDateYear);
+
+      div2.appendChild(eventDetailsHour);
+      div2.appendChild(eventDetailsVenue);
+      div2.appendChild(eventCityCountry);
+
+      div3.appendChild(button);
+
+      li.appendChild(div1);
+      li.appendChild(div2);
+      li.appendChild(div3);
+      ul.appendChild(li);
+
+      eventDateYear.textContent = splitDate(item.start.date, 'year');
+      eventDateMonth.textContent = splitDate(item.start.date, 'month');
+      eventDateDay.textContent = splitDate(item.start.date, 'day');
+
+      eventDetailsHour.textContent = convertDay(item.start.date) + ' - ' + formatTime(item.start.time);
+      eventDetailsVenue.textContent = item.venue.displayName;
+      eventCityCountry.textContent = item.location.city;
+      button.addEventListener('click', function () {
+        //TODO searchLocation(item.venue.id);
+      }, false);
+    });
+
+  });
+}
+
+
 function createListArtist(data) {
 
   const spinner = document.querySelector('.spinner');
@@ -54,8 +140,7 @@ function createListArtist(data) {
           li.classList.add('list-artist-link');
 
           document.querySelector('.list-artist-link').addEventListener('click', function () {
-            //TODO
-            // artistDetails(artist.id);
+            artistDetails(artist.id);
           }, false);
 
         } else {
