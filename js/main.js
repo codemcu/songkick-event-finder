54c047eb3cae966cbe04ab68075f1e18e327cdc6
Mauricio Correa <frontend.mcu@gmail.com>
2018-12-06 14:36:56 +0100

    refactor - createTagAndSetStyle()  _createStructureEventDetail()

--- js/main.js
+++ js/main.js
@@ -72,6 +72,17 @@
     let css = spinner.style;
     (state === 'show') ? css.display = 'block' : css.display = 'none' ;
   }
+  /**
+   * @description crea un nodeElement y le asigna una clase de CSS
+   * @param {string} tagName - nombre de la etiqueta
+   * @param {string} className - nombre de la clase de CSS
+   * @return {HTMLElement}
+   */
+  function createTagAndSetStyle(tagName, className) {
+    const tag = document.createElement(tagName);
+    tag.classList.add(className);
+    return tag;
+  }
 
   /**
    * @description busca la ubicación del evento en los mapas de Google Maps
@@ -161,7 +172,7 @@
    * @description crea una lista con todos los detalles de los eventos del artista
    * @param {number} id - id del artista para buscar todos sus eventos relacionados
    * @param data.totalEntries - numero total de resultados
-   * @param tem.start.date - fecha. Parametro para la funcion splitDate
+   * @param tem.start.date - fecha. Parametro para la función splitDate
    * @return void
    */
   function artistDetails(id) {
@@ -180,12 +191,10 @@
       const container = document.querySelector('.container');
       container.innerHTML = '';
 
-      const eventDetails = document.createElement('DIV');
-      eventDetails.setAttribute('class', 'event-details');
+      const eventDetails = createTagAndSetStyle('DIV', 'event-details');
       container.appendChild(eventDetails);
 
-      const eventTotalSpan = document.createElement('SPAN');
-      eventTotalSpan.setAttribute('class', 'event-total');
+      const eventTotalSpan = createTagAndSetStyle('SPAN', 'event-total');
       eventTotalSpan.textContent = data.totalEntries + ' resultados encontrados';
       eventDetails.appendChild(eventTotalSpan);
 
@@ -195,73 +204,65 @@
 
       listOfEvents.forEach(function (item) {
 
-        const li = document.createElement('LI');
+        _createStructureEventDetail(item, ul);
 
-        const div1 = document.createElement('DIV');
-        div1.setAttribute('class', 'event-date');
-        const eventDateYear = document.createElement('DIV');
-        eventDateYear.setAttribute('class', 'event-date-year');
-        const eventDateMonth = document.createElement('DIV');
-        eventDateMonth.setAttribute('class', 'event-date-month');
-        const eventDateDay = document.createElement('DIV');
-        eventDateDay.setAttribute('class', 'event-date-day');
-
-        const div2 = document.createElement('DIV');
-        div2.setAttribute('class', 'event-date-details');
-        const eventDetailsHour = document.createElement('DIV');
-        eventDetailsHour.setAttribute('class', 'event-date-details-hour');
-        const eventDetailsVenue = document.createElement('DIV');
-        eventDetailsVenue.setAttribute('class', 'event-date-details-venue');
-        const eventCityCountry = document.createElement('DIV');
-        eventCityCountry.setAttribute('class', 'event-date-city-country');
-
-        const div3 = document.createElement('DIV');
-        div3.setAttribute('class', 'event-location');
-        const button = document.createElement('BUTTON');
-        button.appendChild(document.createTextNode('Buscar en el mapa'));
-
-        div1.appendChild(eventDateDay);
-        div1.appendChild(eventDateMonth);
-        div1.appendChild(eventDateYear);
-
-        div2.appendChild(eventDetailsHour);
-        div2.appendChild(eventDetailsVenue);
-        div2.appendChild(eventCityCountry);
-
-        div3.appendChild(button);
-
-        li.appendChild(div1);
-        li.appendChild(div2);
-        li.appendChild(div3);
-        ul.appendChild(li);
-
-        eventDateYear.textContent = splitDate(item.start.date, 'year');
-        eventDateMonth.textContent = splitDate(item.start.date, 'month');
-        eventDateDay.textContent = splitDate(item.start.date, 'day');
-
-        eventDetailsHour.textContent = convertDay(item.start.date) + ' - ' + formatTime(item.start.time);
-        eventDetailsVenue.textContent = item.venue.displayName;
-        eventCityCountry.textContent = item.location.city;
-        button.addEventListener('click', function () {
-          searchLocation(item.venue.id);
-          button.disabled = true;
-        }, false);
       });
 
     });
   }
 
   /**
-   * @description crea un nodeElement y le asigna una clase de CSS
-   * @param {string} tagName - nombre de la etiqueta
-   * @param {string} className - nombre de la clase de CSS
-   * @return {HTMLElement} 
+   * @description crea la estructura con los detalles de cada evento divido por filas, (fecha, ciudad, y botón para buscarlo en el google maps)
+   * @param {Object} item - contiene los detalles del evento seleccionado
+   * @param {HTMLElement} ul - nodo contenedor
+   * @return void
+   * @private
    */
-  function createTagAndSetStyle(tagName, className) {
-    const tag = document.createElement(tagName);
-    tag.classList.add(className);
-    return tag;
+  function _createStructureEventDetail(item, ul) {
+    const li = document.createElement('LI');
+
+    const div1 = createTagAndSetStyle('DIV', 'event-date');
+    const eventDateYear = createTagAndSetStyle('DIV', 'event-date-year');
+    const eventDateMonth = createTagAndSetStyle('DIV', 'event-date-month');
+    const eventDateDay = createTagAndSetStyle('DIV', 'event-date-day');
+
+    const div2 = createTagAndSetStyle('DIV', 'event-date-details');
+    const eventDetailsHour = createTagAndSetStyle('DIV', 'event-date-details-hour');
+    const eventDetailsVenue = createTagAndSetStyle('DIV', 'event-date-details-venue');
+    const eventCityCountry = createTagAndSetStyle('DIV', 'event-date-city-country');
+
+    const div3 = createTagAndSetStyle('DIV', 'event-location');
+    const button = document.createElement('BUTTON');
+    button.appendChild(document.createTextNode('Buscar en el mapa'));
+
+    div1.appendChild(eventDateDay);
+    div1.appendChild(eventDateMonth);
+    div1.appendChild(eventDateYear);
+
+    div2.appendChild(eventDetailsHour);
+    div2.appendChild(eventDetailsVenue);
+    div2.appendChild(eventCityCountry);
+
+    div3.appendChild(button);
+
+    li.appendChild(div1);
+    li.appendChild(div2);
+    li.appendChild(div3);
+    ul.appendChild(li);
+
+    eventDateYear.textContent = splitDate(item.start.date, 'year');
+    eventDateMonth.textContent = splitDate(item.start.date, 'month');
+    eventDateDay.textContent = splitDate(item.start.date, 'day');
+
+    eventDetailsHour.textContent = convertDay(item.start.date) + ' - ' + formatTime(item.start.time);
+    eventDetailsVenue.textContent = item.venue.displayName;
+    eventCityCountry.textContent = item.location.city;
+    button.addEventListener('click', function () {
+      searchLocation(item.venue.id);
+      button.disabled = true;
+    }, false);
   }
+
   /**
    * @description crea la la lista de artistas (ul > li) que coincidan con la búsqueda
    * @param {Response} data - respuesta del servidor
